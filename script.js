dc34418af89759eb1bb33d48c45ca6de127da8a5
helheim0 <helgu77@yahoo.com>
2021-03-31 23:10:26 +0300

    first version

---
+++ script.js
@@ -0,0 +1,24 @@
+function navSlide() {
+    const burger = document.querySelector(".toggle-btn");
+    const nav = document.querySelector(".ul-links");
+    const navLinks = document.querySelectorAll(".ul-links li");
+
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+
+}
+
+navSlide();
